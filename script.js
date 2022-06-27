bd586f920089f89a92bdac5d70cdddb0f54b191e
AdrianKausel <103789648+AdrianKausel@users.noreply.github.com>
2022-06-27 14:20:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function videoPreview(element) {
+    element.play()
+}
+
+function videoPreviewOut(element) {
+    element.pause()
+}
+
+function chngtxt(){
+    document.getElementById("btnS").innerHTML = "Subscribed!";
+}
+
+function chngtxt2(){
+    document.getElementById("btnL").innerHTML = "Logout";
+}
